2c532fa24371172c4773c0350c92b65dbb2bc122
shahan <shahanahmed86@github.com>
2021-02-24 00:30:53 +0500

    error handling

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import session, { Store } from 'express-session';
 import { SESSION_OPTIONS } from './config';
+import { serverError, notFound } from './middleware';
 import { register } from './routes';
 
 export const createApp = (store: Store) => {
@@ -11,6 +12,10 @@ export const createApp = (store: Store) => {
 	app.use(session({ ...SESSION_OPTIONS, store }));
 
 	app.use(register);
+
+	app.use(notFound);
+
+	app.use(serverError);
 
 	return app;
 };
--- api/src/routes/register.ts
+++ api/src/routes/register.ts
@@ -1,24 +1,29 @@
 import { Router } from 'express';
 import { logIn } from '../auth';
-import { guest } from '../middleware';
+import { BadRequest } from '../errors';
+import { catchAsync, guest } from '../middleware';
 import { User } from '../models';
-import { registerSchema } from '../validation';
+import { registerSchema, validate } from '../validation';
 
 const router = Router();
 
-router.post('/register', guest, async (req, res) => {
-	await registerSchema.validateAsync(req.body, { abortEarly: false });
+router.post(
+	'/register',
+	guest,
+	catchAsync(async (req, res) => {
+		await validate(registerSchema, req.body);
 
-	const { email, ...args } = req.body;
+		const { email, ...args } = req.body;
 
-	const found = await User.exists({ email });
-	if (found) return res.status(400).json({ message: 'Invalid Email...' });
+		const found = await User.exists({ email });
+		if (found) throw new BadRequest('Invalid Email...');
 
-	const user = await User.create({ ...args, email });
+		const user = await User.create({ ...args, email });
 
-	logIn(req, user.id);
+		logIn(req, user.id);
 
-	res.json({ message: 'User created successfully' });
-});
+		res.json({ message: 'User created successfully' });
+	})
+);
 
 export default router;
